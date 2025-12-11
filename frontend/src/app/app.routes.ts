e870212420c1d263dad39fe03e5c01d3317c2da0
fernahernandez.c <fernahernandez.c@gmail.com>
2025-12-11 16:08:45 -0300

    Creación de nuevos componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from '@components/home/home';
 import { Auth } from '@components/auth/auth';
 import { Search } from '@components/search/search';
+import { Carrito } from '@components/carrito/carrito';
+import { Account } from '@components/account/account';
 
 export const routes: Routes = [ 
     { path: 'home', component: Home },
     { path: 'auth', component: Auth },
     { path: 'catalogo', component: Search },
+    { path: 'me', component: Account },
+    { path: 'cart', component: Carrito },
     { path: '', redirectTo: 'auth', pathMatch: 'full' }
 ];
